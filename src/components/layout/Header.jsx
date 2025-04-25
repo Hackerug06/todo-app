a975fca3dd48e6503a738c9aed9aab6d66bbf36e
Hackerug06 <hackerug06@gmail.com>
2025-04-25 14:37:02 +0300

    Update Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,1 +1,24 @@
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { Brightness4, Brightness7 } from '@mui/icons-material';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../../app/theme';
 
+const Header = () => {
+  const themeMode = useSelector(state => state.theme.mode);
+  const dispatch = useDispatch();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          To-Do App
+        </Typography>
+        <IconButton onClick={() => dispatch(toggleTheme())} color="inherit">
+          {themeMode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
