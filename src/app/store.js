5e048ccc91d930fc856805dec3e09386791aecc3
Hackerug06 <hackerug06@gmail.com>
2025-04-25 14:34:08 +0300

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tasksReducer from '../features/tasks/tasksSlice';
+import themeReducer from './theme';
 
 export const store = configureStore({
   reducer: {
     tasks: tasksReducer,
+    theme: themeReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: false // For date objects in tasks
     }),
 });
